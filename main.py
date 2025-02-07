9051eb82b37bdbec54f44f68315883f49790e8c4
Amarachi <81161932+Amybundle@users.noreply.github.com>
2025-02-07 02:38:48 +0000

    I am trying to push my day 3 final code

---
+++ main.py
@@ -0,0 +1,13 @@
+print("  The Omega Menu ")
+print("""You'll be asked a bunch of similar yet unrelated questions.
+This determines what we'd have for dinner 😋""")
+print()
+customer_name = input("What's your name? ")
+food_type = input("Name a type of food: ")
+plant_type = input("Name a type of plant: ")
+cooking_method = input("How do you make said food?: ")
+burnt_food = input("A word to describe burned food: ")
+household_item = input("Name a random household item: ")
+print()
+
+print(customer_name,"will be having",cooking_method,food_type,"with",burnt_food,plant_type,"on a bed of",household_item)
